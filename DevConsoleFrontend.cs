e5eff2b2490215d4d4d3a9f61f1d2c2a3a3869b6
Admer <49600504+Admer456@users.noreply.github.com>
2023-03-12 02:00:08 +0100

    Give the connection its own thread

--- DevConsoleFrontend.cs
+++ DevConsoleFrontend.cs
@@ -1,11 +1,43 @@
 ﻿// SPDX-FileCopyrightText: 2023 Admer Šuko
 // SPDX-License-Identifier: MIT
 
+using Elegy.Utilities;
+using Elegy.Utilities.Interfaces;
 using System.Text;
 using ENetConnection = Godot.ENetConnection;
 
 namespace Elegy.DevConsole
 {
+	internal sealed class ConsoleMessage
+	{
+		//public byte[] Message { get; set; } = Array.Empty<byte>();
+		public string Message { get; set; } = string.Empty;
+		public ConsoleMessageType Type { get; set; } = ConsoleMessageType.Info;
+		public float TimeSubmitted { get; set; } = 0.0f;
+	}
+
+	internal sealed class ThreadData
+	{
+		public enum ConnectionMode
+		{
+			Idle,
+			Active,
+			ShuttingDown
+		}
+
+		// If true, main thread will not modify MessageQueue
+		// Controlled by the connection thread
+		public bool IsTransmittingMessages { get; set; }
+
+		// If false, main thread can safely modify MessageQueue
+		// Controlled by the main thread
+		public bool CanTransmitMessages { get; set; }
+
+		public List<ConsoleMessage> MessageQueue { get; set; } = new();
+
+		public ConnectionMode Mode { get; set; } = ConnectionMode.Idle;
+	}
+
 	public class DevConsoleFrontend : IConsoleFrontend
 	{
 		public string Name => "Elegy External Developer Console";
@@ -14,8 +46,15 @@ namespace Elegy.DevConsole
 
 		public bool Initialised { get; set; } = false;
 
+		public DevConsoleFrontend()
+		{
+			mConnectionThread = new Thread( ConnectionThread );
+		}
+
 		public bool Init()
 		{
+			mConnectionThread.Start();
+
 			// In the future, in order to support logging from multiple engine instances, we might wanna have the ability to choose
 			// different ports. E.g. a port range from 23005 to 23015. Also, remote logging possibilities???
 			if ( mConnection.CreateHostBound( bindAddress: "127.0.0.1", bindPort: 23005, maxPeers: 4, maxChannels: 1 ) != Godot.Error.Ok )
@@ -26,55 +65,37 @@ namespace Elegy.DevConsole
 
 			Console.Log( "[DevConsole] Bridge has been established!", ConsoleMessageType.Developer );
 
+			mThreadData.Mode = ThreadData.ConnectionMode.Active;
+
 			Initialised = true;
 			return true;
 		}
 
 		public void OnLog( string message, ConsoleMessageType type, float timeSubmitted )
 		{
-			// A little bit of filtering to ensure all we got are ASCII characters
-			// The external app does not exactly like UTF-16...
-			char[] chars = message.ToCharArray();
-			for ( int i = 0; i < chars.Length; i++ )
+			mMessagesToAdd.Add( new()
 			{
-				if ( !char.IsAscii( chars[i] ) )
-				{
-					chars[i] = '?';
-				}
-			}
-			message = new string( chars );
-
-			mConnection.Broadcast( 0, EncodeMessage( message, type ), (int)Godot.ENetPacketPeer.FlagReliable );
-		}
-
-		private byte[] EncodeMessage( string message, ConsoleMessageType type )
-		{
-			List<byte> data = new( 1 + 1 + 2 + message.Length )
-			{
-				// (M -> message, X -> termination, Q -> query etc.)
-				(byte)'M',					// 1 byte for the packet type
-				(byte)type,					// 1 byte for the console message type
-				(byte)message.Length,		// 2 bytes for the length of the message
-				(byte)(message.Length >> 8)	// N bytes for the actual text
-			};
-			data.AddRange( Encoding.ASCII.GetBytes( message ) );
-
-			return data.ToArray();
+				//Message = Encoding.ASCII.GetBytes( message ),
+				Message = message,
+				TimeSubmitted = timeSubmitted,
+				Type = type
+			} );
 		}
 
 		public void OnUpdate( float delta )
 		{
-			//mConnection.Flush();
-			while ( true )
+			mTimeToNextTransmission -= delta;
+			if ( mTimeToNextTransmission > 0.0f || mThreadData.IsTransmittingMessages )
 			{
-				var result = mConnection.Service();
-				if ( result[0].AsInt32() <= (int)ENetConnection.EventType.None )
-				{
-					break;
-				}
-
-				LogEvent( result );
+				return;
 			}
+
+			mThreadData.CanTransmitMessages = false;
+			mThreadData.MessageQueue.AddRange( mMessagesToAdd );
+			mMessagesToAdd.Clear();
+			mThreadData.CanTransmitMessages = true;
+
+			mTimeToNextTransmission = 0.5f;
 		}
 
 		private void LogEvent( Godot.Collections.Array? serviceResult )
@@ -104,29 +125,98 @@ namespace Elegy.DevConsole
 			}
 		}
 
+		private static byte[] EncodeMessage( string message, ConsoleMessageType type )
+		{
+			ByteBuffer buffer = new( 1 + 1 + 2 + message.Length );
+			// 1 byte for the packet type
+			buffer.WriteChar( 'M' );
+			// 1 byte for the console message type
+			buffer.Write( type );
+			// 2 + N bytes for string length and string contents
+			buffer.WriteStringAscii( message, StringLength.Medium );
+
+			return buffer.Data.ToArray();
+		}
+
+		private void ConnectionThread( object? obj )
+		{
+			bool keepRunning = true;
+			while ( keepRunning )
+			{
+				switch ( mThreadData.Mode )
+				{
+					case ThreadData.ConnectionMode.Idle:
+						{
+							Thread.Sleep( 100 );
+						} break;
+
+					case ThreadData.ConnectionMode.Active:
+						{
+							if ( mThreadData.CanTransmitMessages && mThreadData.MessageQueue.Count > 0 )
+							{
+								mThreadData.IsTransmittingMessages = true;
+
+								for ( int i = 0; i < mThreadData.MessageQueue.Count; i++ )
+								{
+									ConsoleMessage message = mThreadData.MessageQueue[i];
+									mConnection.Broadcast( 0, EncodeMessage( message.Message, message.Type ), (int)Godot.ENetPacketPeer.FlagReliable );
+								}
+								mThreadData.MessageQueue.Clear();
+
+								mThreadData.IsTransmittingMessages = false;
+							}
+
+							while ( true )
+							{
+								var result = mConnection.Service();
+								if ( result[0].AsInt32() <= (int)ENetConnection.EventType.None )
+								{
+									break;
+								}
+								
+								LogEvent( result );
+							}
+
+							Thread.Sleep( 100 );
+						} break;
+
+					case ThreadData.ConnectionMode.ShuttingDown:
+						{
+							mConnection.Broadcast( 0, new byte[] { (byte)'X' }, (int)Godot.ENetPacketPeer.FlagReliable );
+							for ( int i = 0; i < 64; i++ )
+							{
+								LogEvent( mConnection.Service( 1 ) );
+							}
+
+							var peers = mConnection.GetPeers();
+							foreach ( var peer in peers )
+							{
+								peer.PeerDisconnectNow();
+							}
+							mConnection.Service();
+
+							mPeerMap.Clear();
+
+							keepRunning = false;
+						} break;
+				}
+			}
+		}
+
 		public void Shutdown()
 		{
 			Console.Log( $"[DevConsole] Shutdown" );
 			Initialised = false;
 
-			mConnection.Broadcast( 0, new byte[] { (byte)'X' }, (int)Godot.ENetPacketPeer.FlagReliable );
-			for ( int i = 0; i < 64; i++ )
-			{
-				LogEvent( mConnection.Service( 1 ) );
-			}
-
-			var peers = mConnection.GetPeers();
-			foreach ( var peer in peers )
-			{
-				peer.PeerDisconnectNow();
-			}
-			mConnection.Service();
-
-			mPeerMap.Clear();
-			//mConnection.Destroy();
+			mThreadData.Mode = ThreadData.ConnectionMode.ShuttingDown;
+			mConnectionThread.Join();
 		}
 
 		private Dictionary<Godot.ENetPacketPeer, string> mPeerMap = new();
 		private ENetConnection mConnection = new();
+		private Thread mConnectionThread;
+		private ThreadData mThreadData = new();
+		private List<ConsoleMessage> mMessagesToAdd = new();
+		private float mTimeToNextTransmission = 0.2f;
 	}
 }
